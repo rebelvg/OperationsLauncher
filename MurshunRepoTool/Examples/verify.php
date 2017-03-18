8e43e9a85b20ea2f354f09c231aa0fd47d5ff24b
Rebel <johnhartigan@ya.ru>
2017-03-18 06:50:01 +0200

    no message

--- MurshunRepoTool/Examples/verify.php
+++ MurshunRepoTool/Examples/verify.php
@@ -2,14 +2,16 @@
 $password = "REPO_PASSWORD";
 $file = "verify.txt";
 
-if (!empty($_GET['md5']) && !empty($_GET['password'])) {
-	if ($_GET['password'] == $password) {
-		$data = $_GET['md5'];
+echo @file_get_contents($file);
 
-		file_put_contents($file, $data);
-	};
-} else {
-	echo file_get_contents($file);
-};
+if (empty($_GET['md5']) || empty($_GET['password']))
+    exit();
+
+if ($_GET['password'] !== $password)
+    exit();
+
+$data = $_GET['md5'];
+
+@file_put_contents($file, $data);
 
 ?>
