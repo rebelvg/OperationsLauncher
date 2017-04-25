1ea3e796adbe3c7fcb22f67fd49765055c1aab49
Rebel <johnhartigan@ya.ru>
2017-04-26 02:47:48 +0300

    no message

--- OperationsUploader/MurshunUploader/Form1.Designer.cs
+++ OperationsUploader/MurshunUploader/Form1.Designer.cs
@@ -103,6 +103,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.password_textBox);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
