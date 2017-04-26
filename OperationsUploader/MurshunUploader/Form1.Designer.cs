690f5d24c47dcc58879c2922df71e0361686747e
Rebel <johnhartigan@ya.ru>
2017-04-26 06:33:14 +0300

    no message

--- OperationsUploader/MurshunUploader/Form1.Designer.cs
+++ OperationsUploader/MurshunUploader/Form1.Designer.cs
@@ -105,6 +105,7 @@
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Operations Uploader";
