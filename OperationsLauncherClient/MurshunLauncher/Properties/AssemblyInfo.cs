287a0d5e1942d638f90e723529df73288b0f3ee3
Rebel <johnhartigan@ya.ru>
2017-04-27 14:53:14 +0300

    no message

--- OperationsLauncherClient/MurshunLauncher/Properties/AssemblyInfo.cs
+++ OperationsLauncherClient/MurshunLauncher/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MurshunLauncher")]
+[assembly: AssemblyProduct("OperationsLauncher")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- OperationsUploader/MurshunUploader/Properties/AssemblyInfo.cs
+++ OperationsUploader/MurshunUploader/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MurshunUploader")]
+[assembly: AssemblyProduct("OperationsUploader")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
