6b127cdb3cc1a95bdc260448e7f8af104f9cc64a
Rebel <johnhartigan@ya.ru>
2018-06-08 21:51:20 +0300

    Changed button name.

--- OperationsUploader/MurshunUploader/Form1.Designer.cs
+++ OperationsUploader/MurshunUploader/Form1.Designer.cs
@@ -57,11 +57,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(208, 314);
+            this.label1.Location = new System.Drawing.Point(220, 314);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Password ->";
+            this.label1.Text = "Password";
             // 
             // label2
             // 
